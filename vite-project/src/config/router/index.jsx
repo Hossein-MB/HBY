640abfdc41955f42a01833cdeda6c6d7bf8698c3
Yasin <abdyassin1391@gmail.com>
2025-11-01 19:33:50 +0330

    vv

--- vite-project/src/config/router/index.jsx
+++ vite-project/src/config/router/index.jsx
@@ -1,2 +1,18 @@
-import createBrowserRouter from 'react-router-dom'
-import React from 'react'
+import { createBrowserRouter } from "react-router-dom";
+import React, { Children, Component } from "react";
+import RegisterPage from "../../screens/registerPage";
+import MainBox from "../../components/register/MainBox";
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RegisterPage,
+    children: [
+      {
+        index:true,
+        path:"step1",
+        Component: MainBox,
+      },
+    ],
+  },
+]);
+export { Router };
--- vite-project/src/screens/registerPage/index.jsx
+++ vite-project/src/screens/registerPage/index.jsx
@@ -3,13 +3,25 @@ import React from "react";
 import Icon from "../../components/common/AuthHeader/Icon";
 import AuthHeader from "../../components/common/AuthHeader";
 import MainBox from "../../components/register/MainBox";
+import { Outlet } from "react-router-dom";
 const RegisterPage = () => {
   return (
     <>
+      <div className="-z-2 bg-[#9B0EE1] w-full h-full absolute top-0 bottom-0"></div>
+      <img className="-z-1 absolute left-0 top-0 w-full h-full" src="register/image.png"/>
       
-      <AuthHeader/>
-     
-      <MainBox />
+      <AuthHeader />
+      <img
+        src="register/register_page_sth.png"
+        className="w-[550px] h-[550px] mt-[30px] ml-[160px] inline"
+      />
+      <button className="cursor-pointer text-white font-[700] w-[130px]  font-vazir h-[48px] relative bottom-28 right-14 text-2md/1 rounded-[16px] bg-[#3C8B85]">
+        ورود
+      </button>
+      <div className="float-right  font-vazir mr-[200px] mt-[164px]">
+        <Outlet/>
+      </div>
+      
     </>
   );
 };
