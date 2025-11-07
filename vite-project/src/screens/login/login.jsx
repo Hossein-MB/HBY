e0e71a9523ca835b6a9d778ba2bd64c3a47f1247
fateme <fateme.ahmadi.asouri.1983@gmail.com>
2025-11-07 17:35:55 +0330

    d

---
+++ vite-project/src/screens/login/login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class login extends Component {
+  render() {
+    return (
+      <div>login</div>
+      
+    )
+  }
+}
+
+export default login
