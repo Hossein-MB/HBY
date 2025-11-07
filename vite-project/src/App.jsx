8026b510cc3e3997497feba124370049a67d4007
fateme <fateme.ahmadi.asouri.1983@gmail.com>
2025-11-07 10:13:10 +0330

    login

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,15 @@
 import HeaderComponent from "./Components/LandingComponent/HeaderComponent/HeaderComponent";
+import { Login } from "./screens/loginpage/Login";
 
 export default function App() {
   return (
     <>
       <h1>testtt</h1>
 
-      <div className="w-full  bg-black flex justify-evenly ">
+      <div className="w-full  flex justify-evenly ">
         <HeaderComponent />
       </div>
+        <Login/>
     </>
   );
 }
