9d1eaf1b5775ea3839518b9fd16e39126ad6c3d0
Bahar <baharbetvan@gmail.com>
2025-11-08 14:00:55 +0330

    login-link

--- vite-project/src/Components/LandingComponent/HeaderComponent/LoginComponent.jsx
+++ vite-project/src/Components/LandingComponent/HeaderComponent/LoginComponent.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom"; 
 
 const LoginComponent = () => {
-  return (
-    <div class="space-end w-[150px] h-[40px] pr-[23px] pt-[6px] pl-[23px] gap-[8px] bg-[#9B0EE1] rounded-md text-amber-50">
-      <div>ورود/ثبت نام</div>
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default LoginComponent
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className="flex justify-center items-center w-[150px] h-[40px] pr-[23px] pt-[6px] pl-[23px] gap-[8px] bg-[#9B0EE1] rounded-md text-amber-50">
+      <button onClick={handleLoginClick}>ورود/ثبت نام</button>
+    </div>
+  );
+};
+
+export default LoginComponent;
